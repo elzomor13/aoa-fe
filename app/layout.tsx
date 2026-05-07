a21cf83a3fb2f00282ddd63ea1bc0937e0754bec
mohamedelzomor <elzomor.online@gmail.com>
2026-05-07 04:34:42 +0300

    add logo to the url

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,9 @@
+import type { Metadata } from 'next';
 import './globals.css';
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://aoa.aoa-online.uk'),
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return children;
